7d80097250bd8997d05f8150f30f0b6bad507cd3
Pupsikk <orativtech7@gmail.com>
2021-02-21 18:13:11 +0200

    First try

---
+++ Cat.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def get_sex(self):
+        return self.sex
---
+++ main.py
@@ -0,0 +1,4 @@
+from Cat import Cat
+
+cat1 = Cat('Барон', 2, 'Мальчик')
+cat2 = Cat('Сэм', 2, 'Мальчик')
